83ca164222ae4eed3e00256d3b149ec6554b0c25
Durvz Maia <89949017+Durvalm@users.noreply.github.com>
2022-07-14 16:10:59 -0400

    created review engine

--- apps/saloons/models.py
+++ apps/saloons/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import Avg, Count
+from apps.users.models import User
 
 class Saloon(models.Model):
     """Model that takes care of each saloon/franchise"""
@@ -21,4 +23,31 @@ class Saloon(models.Model):
         always remember to divide address by spaces in the database"""
         sliced = self.adress.split(' ')
         return sliced
+    
+    def average_review(self):
+        """Computer the average review of each saloon"""
+        reviews = Review.objects.filter(saloon=self).aggregate(average=Avg('rating'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+        return avg
 
+    def review_count(self):
+        """Compute how many reviews the saloon has"""
+        reviews = Review.objects.filter(saloon=self).aggregate(count=Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    saloon = models.ForeignKey(Saloon, on_delete=models.CASCADE)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.review[0:15]
