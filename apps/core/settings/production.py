6baff115fc698c166227392f679ba1babbdd0432
Durvz Maia <89949017+Durvalm@users.noreply.github.com>
2022-11-14 20:53:33 -0500

    fixed db

--- apps/core/settings/production.py
+++ apps/core/settings/production.py
@@ -4,6 +4,7 @@ import dj_database_url
 # DATATABASE
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+DATABASES['default']['ENGINE'] = 'django_tenants.postgresql_backend'
 print(DATABASES)
 
 DEBUG = True
