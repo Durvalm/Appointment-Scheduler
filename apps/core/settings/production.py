023841b89097d47e06985cb5d8198e4f8a96afa6
Durvz Maia <89949017+Durvalm@users.noreply.github.com>
2022-11-14 20:44:19 -0500

    print db

--- apps/core/settings/production.py
+++ apps/core/settings/production.py
@@ -4,6 +4,7 @@ import dj_database_url
 # DATATABASE
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+print(DATABASES)
 
 DEBUG = True
 ALLOWED_HOSTS = ['superbarber.site', 'saloon.lol', 'https://superbarberapp.herokuapp.com/']
